e20acf64124791e5461a5f5e9bf003792a2b68b4
Jon Miller <jonEbird@gmail.com>
2020-04-07 20:53:25 -0400

    setup.py: removing setuptools_git and pip.req trick
    I am not currently tagging anything and this is such a small, simple
    project that I do not need to use `setuptools_git`, nor do I care to
    keep up with whether it is even still maintained.
    
    The trick of using pip to parse and collect my requirements ends up
    being bad since it is using unpublished internals which actually broke
    quite a few pip releases ago.

--- setup.py
+++ setup.py
@@ -6,37 +6,12 @@ import os
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
-# Cool trick to automatically pull in install_requires values directly from
-# your requirements.txt file but you need to have pip module available
-try:
-    from pip.req import parse_requirements
-    try:
-        # Newer versions of pip need a session object when parsing requirements.
-        from pip.download import PipSession
-        install_reqs = parse_requirements(os.path.join(here, 'requirements.txt'),
-                                          session=PipSession())
-    except ImportError:
-        # Must be an older version of pip
-        install_reqs = parse_requirements(os.path.join(here, 'requirements.txt'))
-
-    # reqs is a list of requirement
-    # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-    reqs = [str(ir.req) for ir in install_reqs]
-except Exception as e:
-    print("Failed to parse requirements: " + str(e))
-    # I suggest you install pip on your system so you don't have to
-    # manually update this list along with your requirements.txt file
-    reqs = []
-
 try:
     scripts = ['scripts/%s' % f for f in os.listdir('scripts') if f != "dummy.py"]
 except OSError:
     scripts = []
 
-# By requiring the setuptools_version_command package we can specify our
-# version via the version_command= argument. The version is determined from your git tags.
-setup_extra = {'version_command': ('git describe --tags --dirty', 'pep440-git')}
-
+version = "0.1.0"
 
 setup(name='strace-summary',
       description="Help summarize strace data",
@@ -49,20 +24,16 @@ setup(name='strace-summary',
       author_email="jonEbird@gmail.com",
       url="https://github.com/jonEbird/strace-summary",
       license="Apache",
+      version=version,
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       scripts=scripts,
-      # Note an explicit pep8 version is required here in order for flake8 to
-      # to work (as it has specific version requirements that are not directly
-      # specified). Hopefully this will be resolved in the future.
-      setup_requires=["setuptools_git", "setuptools_version_command",
-                      "flake8", "pep8 == 1.5.7", "nose"],
+      setup_requires=["flake8", "pytest-runner", "pytest"],
       dependency_links=[],
-      tests_require=['nose', 'coverage'],
+      tests_require=['pytest', 'coverage'],
       zip_safe=True,
-      install_requires=reqs,
+      install_requires=[],
       entry_points="""
       # -*- Entry points: -*-
       """,
-      **setup_extra
       )
